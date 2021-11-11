ca213f4e34362a376444fea5edc9c292ef735a4e
Roger Martin <rdmartin33@hotmail.com>
2021-11-11 16:39:15 -0700

    Send email if anything about the page changes

---
+++ WebScraper/WebScrape.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace WebScraper;
+
+public class WebScrape
+{
+    public WebScrape(string id, string hash)
+    {
+        Id = id;
+        Hash = hash;
+    }
+
+    [JsonProperty(PropertyName = "id")]
+    public string Id { get; set; }
+
+    [JsonProperty(PropertyName = "hash")]
+    public string Hash { get; set; }
+
+    public override string ToString()
+    {
+        return JsonConvert.SerializeObject(this);
+    }
+}
--- WebScraper/WebScraper.cs
+++ WebScraper/WebScraper.cs
@@ -1,6 +1,9 @@
+using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using SendGrid.Helpers.Mail;
 using System;
+using System.Linq;
 using System.Net.Http;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
@@ -10,17 +13,54 @@ namespace WebScraper;
 public static class WebScraper
 {
     [FunctionName("WebScraper")]
-    public static async Task RunAsync([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+    public static async Task RunAsync(
+        [TimerTrigger("0 */5 * * * *")]TimerInfo myTimer,
+        [CosmosDB(databaseName: "tis-cosmos-db", containerName: "web-scraper", Connection = "CosmosDbConnectionString")]
+        CosmosClient cosmosClient,
+        [SendGrid(ApiKey = "TisSendGridApiKey")] IAsyncCollector<SendGridMessage> messageCollector,
+        ILogger log)
     {
-        // log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-
-        var client = new HttpClient();
-        var pageContent = await client.GetStringAsync("https://boulderhousing.org/now-renting");
+        var httpClient = new HttpClient();
+        var pageContent = await httpClient.GetStringAsync("https://boulderhousing.org/now-renting");
 
         using (SHA256 mySHA256 = SHA256.Create())
         {
-            var hash = BitConverter.ToString(mySHA256.ComputeHash(System.Text.Encoding.Default.GetBytes(pageContent)));
-            log.LogInformation($"Hash: {hash}");
+            if (!string.IsNullOrEmpty(pageContent))
+            {
+                var hash = BitConverter.ToString(mySHA256.ComputeHash(System.Text.Encoding.Default.GetBytes(pageContent)));
+
+                log.LogInformation($"INFO: Hash: {hash}");
+                var container = cosmosClient.GetDatabase("tis-cosmos-db").GetContainer("web-scraper");
+                var queryDefinition = new QueryDefinition("SELECT * FROM items");
+                using (var resultSet = container.GetItemQueryIterator<WebScrape>(queryDefinition))
+                {
+                    while (resultSet.HasMoreResults)
+                    {
+                        FeedResponse<WebScrape> response = await resultSet.ReadNextAsync();
+                        WebScrape dbItem = response.First();
+
+                        if (dbItem.Hash == hash)
+                        {
+                            log.LogInformation("INFO: No changes to web page");
+                        } else
+                        {
+                            log.LogInformation("INFO: Web page has changed");
+                            var newDbItem = new WebScrape(dbItem.Id, hash);
+                            var dbResponse = await container.ReplaceItemAsync(newDbItem, newDbItem.Id);
+
+                            var message = new SendGridMessage();
+                            message.AddTo("rdogmartin@gmail.com");
+                            message.AddContent("text/html", "There may be a new rental at https://boulderhousing.org/now-renting");
+                            message.SetFrom(new EmailAddress("roger@techinfosystems.com"));
+                            message.SetSubject("INFO: Web page has changed");
+
+                            await messageCollector.AddAsync(message);
+                        }
+                    }
+                }
+
+                log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            }
         }
     }
 }
